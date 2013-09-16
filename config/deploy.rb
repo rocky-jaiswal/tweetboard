02cb971e7ea76f84d69301d6dd1c13dc376f54fd
rocky-jaiswal <rocky.jaiswal@gmail.com>
2013-09-16 21:48:29 +0530

    Trying torquebox capistrano support

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'torquebox-capistrano-support'
+
 set :application, "tweetboard"
 set :repository,  "https://github.com/rocky-jaiswal/tweetboard"
 set :scm, :git
@@ -9,5 +11,25 @@ role :web, "192.168.1.155"                          # Your HTTP server, Apache/e
 role :app, "192.168.1.155"                          # This may be the same as your `Web` server
 role :db,  "192.168.1.155", :primary => true        # This is where Rails migrations will run
 
+
+set :torquebox_home, "/opt/torquebox/current"
+set :rails_env, "production"
+set :app_context, "/"
+
+
+namespace :torquebox do
+  task :deploy do
+    puts "==================Stop and Undeploy======================"
+    run  "cap deploy:torquebox:stop"
+    run  "rm -rf #{torquebox_home}/standalone/deployments/*"
+    puts "==================Bundle======================"
+    run  "cd #{deploy_to}/current && bundle install --deployment --without development test"
+    puts "==================Deploy and Run======================"
+    run  "cd #{deploy_to}/current && torquebox deploy"
+    run  "cd #{deploy_to}/current && cap deploy:torquebox:start"
+  end
+end
+
+after "deploy", "torquebox:deploy"
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
