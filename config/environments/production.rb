a4fc67bc7063a328a41d3ead746bccea45122b7e
rocky-jaiswal <rocky.jaiswal@gmail.com>
2013-09-17 23:59:52 +0530

    confirmation emails not on https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ TweetBoard::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'tweetboard.in' }
+  config.action_mailer.default_url_options = { :protocol => 'https', :host => 'tweetboard.in' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
